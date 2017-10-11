e322c1ec5f178a9270284efbe19729bfea563500
Rikesh Dhokia <rikeshdhokia@gmail.com>
2017-10-11 18:46:28 +1100

    add Home component

--- src/app.js
+++ src/app.js
@@ -1,61 +1,9 @@
 import React from 'react';
-import {
-  Text, View, Image
-} from 'react-native';
-import MovieBoard from './resources/movieBoardImg.jpg';
-import MovieList from './components/MovieList';
 
-const Home = () => (
-  <View style={styles.home}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>
-          Random Movie Finder
-        </Text>
-      </View>
-      <View style={styles.homeContent}>
-        <Text>
-          What movie should we watch tonight?
-        </Text>
-        <Image style={styles.image} source={MovieBoard} />
-      </View>
-      <View>
-        <MovieList />
-      </View>
-  </View>
-);
+import Home from './components/Home';
+
 const App = () => (
   <Home />
 );
-
-const styles = {
-  home: {
-    flex: 1
-  },
-  headerText: {
-    textAlign: 'center',
-    fontSize: 16
-  },
-  header: {
-    backgroundColor: '#F8F8F8',
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-  },
-  image: {
-    width: '30%',
-    height: '30%',
-    resizeMode: 'contain',
-
-  },
-  homeContent: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-};
 
 export default App;
